83372cbc20c8ede1522cdbd1b89ee001c29745be
mandeep kashyap <mandeepkkr140999@gmail.com>
2025-12-25 16:33:34 +0530

    somthing change

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    }
+);
+
+const uploadToCloudinary = async (localFilePath) => {
+    try {
+       if(!localFilePath) return null;
+       // Uploads file to Cloudinary
+         const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+         });
+        // file uploaded to cloudinary successfully
+        console.log("file uploaded to cloudinary successfully", result.url);
+        return result;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // delete the local file in case of error
+        console.log("Error while uploading to cloudinary", error);
+        // throw error;
+        return null;
+    }
+}
